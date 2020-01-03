7147680819bc1a59925037f2b3d83d2bb9bfcd50
alex_klymenko <klymenko.sasha21@gmail.com>
2020-01-03 12:42:49 +0200

    - initial styles - implement UI for the game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,11 @@ import {
   OnInit
 } from '@angular/core';
 
+interface cardInterface {
+  value: number;
+  isEnabled: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,9 +16,21 @@ import {
 export class AppComponent implements OnInit {
 
   title = 'Mahjong-like game';
+  cardArray: cardInterface[] = [
+    {value: 0, isEnabled: false},
+    {value: 1, isEnabled: false},
+    {value: 2, isEnabled: false},
+    {value: 3, isEnabled: false},
+    {value: 4, isEnabled: false},
+    {value: 5, isEnabled: false},
+  ];
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  showCard(item: cardInterface): void {
+    item.isEnabled = !item.isEnabled;
+  }
 
 }
